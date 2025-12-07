229bdada0363488473179187b35f5322384f12a9
Behruz <behruzbekzokirov17@gmail.com>
2025-12-07 23:02:06 +0500

    Add files via upload

---
+++ data.py
@@ -0,0 +1,73 @@
+question_data = [
+
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science & Nature",
+        "question": "The Earth revolves around the Sun in one year.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science & Nature",
+        "question": "Not including false teeth; A human has two sets of teeth in their lifetime.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science & Nature",
+        "question": "Celiac Disease is a disease that effects the heart, causing those effected to be unable to eat meat.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science & Nature",
+        "question": "The Earth and the Moon roughly have the same mass.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science & Nature",
+        "question": "A plant that has a life cycle for more than a year is known as an annual.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science & Nature",
+        "question": "An atom contains a nucleus.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science & Nature",
+        "question": "Psychology is the science of behavior and mind.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    }
+]
---
+++ question_model.py
@@ -0,0 +1,7 @@
+class Question:
+    def __init__(self, q_text, a_text):
+        self.text = q_text
+        self.answer = a_text
+
+
+
